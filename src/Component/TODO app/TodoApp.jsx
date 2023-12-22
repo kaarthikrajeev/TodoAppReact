d5f01816edb1ea2ea943341f6a7ca21b8a97c718
kaarthikraj <karthick77r@gmail.com>
2023-12-22 12:51:19 +0530

    add input functionality

--- src/Component/TODO app/TodoApp.jsx
+++ src/Component/TODO app/TodoApp.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 import "./TodoApp.css";
 export default class TodoApp extends Component {
+  state={
+    input:"",
+    items:[]
+  }
+  handleChange=(event)=>{
+    this.setState({
+      input:event.target.value
+    });
+  };
+  storeItems= event =>{
+    event.preventDefault();
+    const { input }=this.state;
+    this.setState({
+      items:[...this.state.items,input],
+      input:""
+    })
+  }
+
   render() {
+    const { input, items } =this.state;
+    
     return (
       <div className='todo-container'>
-        <form className='input-section'>
+        <form className='input-section' onSubmit={this.storeItems}>
             <h1>TodoApp</h1>
-            <input type="text" placeholder='Enter Items...' />
+            <input type="text" value={input} onChange={this.handleChange} placeholder='Enter Items...' />
         </form>
         <ul>
-            <li>items</li>
+            {items.map((data,index)=>(
+              <li key={index}>{data} <i class="fas fa-trash-alt"></i></li>
+            ))}
         </ul>
         </div>
     )
